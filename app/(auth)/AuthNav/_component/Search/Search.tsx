95097bf793dca568f0d55b8ee61a04976498df63
golddick <63362187+golddick@users.noreply.github.com>
2024-05-28 18:30:32 +0100

    updated nav

--- app/(auth)/AuthNav/_component/Search/Search.tsx
+++ app/(auth)/AuthNav/_component/Search/Search.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Input } from '../../../ui/input'
 import { SearchIcon } from 'lucide-react'
-import { Button } from '../../../ui/button'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 const Search = () => {
   return (
