866b370629717a910e838013db1d7ee5ef0ea28d
golddick <63362187+golddick@users.noreply.github.com>
2024-07-18 20:12:57 +0100

    new

--- components/StreamHome/Stream.tsx
+++ components/StreamHome/Stream.tsx
@@ -71,7 +71,7 @@ const Stream = () => {
   ];
 
   return (
-    <div className='grid py-2 gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-full px-2'>
+    <div className='grid py-2 gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 h-full px-2'>
       {streamData.map((stream) => (
         <StreamItem key={stream.id} stream={stream} />
       ))}
